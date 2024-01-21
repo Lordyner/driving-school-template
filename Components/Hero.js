212587bf19b335a640ad2295e16d8fad2b4b297d
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-01-21 10:41:16 +0100

    Add alternative image for hero section on mobile

--- Components/Hero.js
+++ Components/Hero.js
@@ -1,12 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useContext, useRef } from 'react';
 import classes from './Hero.module.css';
 import Image from 'next/image';
 import heroImg from '@/public/images/hero_image.png';
-import { Grand_Hotel } from 'next/font/google';
+import mobileHeroImg from '@/public/images/hero_image_mobile.png';
+import GlobalContext from '@/Store/GlobalContext';
 
 
 const Hero = () => {
-
+    const { isMobileResolution } = useContext(GlobalContext);
     return (
         <section id="hero" className={classes.heroContainer}>
             <h1>Apprendre<br /> à bien <span className='underline'>conduire</span></h1>
@@ -19,7 +20,7 @@ const Hero = () => {
             </div>
             <div className={classes.imgWrapper}>
 
-                <Image src={heroImg} alt='moniteur et son élève dans une voiture en train de réviser' className={classes.heroImg} />
+                <Image src={isMobileResolution ? mobileHeroImg : heroImg} alt='moniteur et son élève dans une voiture en train de réviser' className={classes.heroImg} />
             </div>
         </section >
     );
