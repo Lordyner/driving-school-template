e8df86bbbfbd545e458b2b631f286f81abbcf154
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-01-22 18:43:51 +0100

    Minor UI changes for mobile review section

--- Components/Reviews.js
+++ Components/Reviews.js
@@ -4,7 +4,8 @@ import Review from './Review';
 const Reviews = ({ reviews }) => {
 
     return (
-        <section>
+        <section className={classes.reviewSection}>
+            <h2><span className='underline'>Ce</span> que nos élèves <span className='underline gradient'>pensent de nous</span></h2>
             {reviews && reviews.length < 0 && <p>En cours</p>}
             {reviews && reviews.length > 0 && <div className={classes.reviews}>
                 {reviews.map((review, index) => (
